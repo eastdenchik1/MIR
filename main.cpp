0f39a4448a4970d7e52eba58e72595ad96f1ed6a
eastdenchik1 <32023322+eastdenchik1@users.noreply.github.com>
2019-03-21 20:47:42 +0300

    UPLOAD 2.0

--- main.cpp
+++ main.cpp
@@ -16,7 +16,9 @@ int main(int argc, char ** argv){
                cout << line << endl;
             }
             fin.close();
-        } else {
+        } else if (strcmp(argv[1],"--version")==0) {
+            cout << argv[0] << " version - 1.0" << endl; return 0;
+         } else {
             MIR obj = MIR(argv[1]);
             obj.get_without_keys();
         }
